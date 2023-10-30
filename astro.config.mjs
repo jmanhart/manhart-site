3de06749ee098dfb6a4ff6942efee10abbce85ea
John Manhart <manhartjohn@gmail.com>
2023-10-30 14:44:27 -0700

    Adding in Sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,26 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import node from "@astrojs/node";
 
-import sitemap from '@astrojs/sitemap';
+import sentry from "@sentry/astro";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    sentry({
+      dsn: "https://7468cd688dfc227a1af9a98dc502857a@o4506140624879616.ingest.sentry.io/4506140741730304",
+      sourceMapsUploadOptions: {
+        project: "opposite-orbit",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ],
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
