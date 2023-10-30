a3fb36590ac19287e81d4a3a1dfe54704ef3b7a3
John Manhart <manhartjohn@gmail.com>
2023-10-30 15:36:35 -0700

    eh 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import node from "@astrojs/node";
 
-import sentry from "@sentry/astro";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), sentry()],
+  integrations: [mdx(), sitemap()],
   output: "server",
   adapter: node({
     mode: "standalone",
