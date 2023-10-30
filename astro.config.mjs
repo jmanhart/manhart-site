ff219db89c66f55fd0de09b3a8cadd2ceadd8202
John Manhart <manhartjohn@gmail.com>
2023-10-30 16:46:37 -0700

    eh 12

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,7 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import node from "@astrojs/node";
 import vercelStatic from "@astrojs/vercel/static";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://manhart.io",
-  integrations: [mdx(), sitemap()],
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  output: "static",
+  adapter: vercelStatic(),
 });
