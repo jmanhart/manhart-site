d6697709a97eb7f1cff8f94499e0606ff5dd65f0
John Manhart <manhartjohn@gmail.com>
2023-10-30 15:24:40 -0700

    revert and ripping out sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,13 +8,7 @@ import sentry from "@sentry/astro";
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [
-    mdx(),
-    sitemap(),
-    sentry({
-      dsn: "https://7468cd688dfc227a1af9a98dc502857a@o4506140624879616.ingest.sentry.io/4506140741730304",
-    }),
-  ],
+  integrations: [mdx(), sitemap(), sentry()],
   output: "server",
   adapter: node({
     mode: "standalone",
