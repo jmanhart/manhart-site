fc1804822a8649d8ea73fbb1175f1b92b1942ae1
John Manhart <manhartjohn@gmail.com>
2023-10-30 16:29:30 -0700

    eh 8

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import node from "@astrojs/node";
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
