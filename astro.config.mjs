a08b2c23422e56a13064f07dd48cea99bb8d4d20
John Manhart <manhartjohn@gmail.com>
2023-12-06 12:21:30 -0800

    Adding in spotlight to see how it rolls

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from "astro/config";
 import vercelStatic from "@astrojs/vercel/static";
 import sitemap from "@astrojs/sitemap";
 
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: vercelStatic(),
-  integrations: [sitemap()]
+  integrations: [sitemap(), sentry(), spotlightjs()]
 });
