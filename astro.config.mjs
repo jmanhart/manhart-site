77020083cc01a862b64aadf52ff37adb089d587e
John Manhart <manhartjohn@gmail.com>
2024-11-16 20:37:41 -0800

    The great purge

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,9 @@ import { defineConfig } from "astro/config";
 import vercelStatic from "@astrojs/vercel/static";
 import sitemap from "@astrojs/sitemap";
 
-import sentry from "@sentry/astro";
-import spotlightjs from "@spotlightjs/astro";
-
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: vercelStatic(),
-  integrations: [sitemap(), sentry(), spotlightjs()]
+  integrations: [sitemap()],
 });
