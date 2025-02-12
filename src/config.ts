dfb382656de600f0f2ebf28e79a8ea67ff7f47bd
John Manhart <manhartjohn@gmail.com>
2025-02-11 20:08:50 -0800

    Cleaning up - Favicon, mobile stuff, new icons

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Manhart Site';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "manhart.io";
+export const SITE_DESCRIPTION = "Welcome to my website!";
 
-export const SITE = 'manhart.io';
+export const SITE = "manhart.io";
