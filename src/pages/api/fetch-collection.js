372606a1643f057862c5059f8dcd5763dba75e81
John Manhart <manhartjohn@gmail.com>
2025-02-16 17:57:25 -0800

    bumo

--- src/pages/api/fetch-collection.js
+++ src/pages/api/fetch-collection.js
@@ -1,12 +1,12 @@
-import { fetchCollection } from "../../utils/discogsApi.js";
+// import { fetchCollection } from "../../utils/discogsApi.js";
 
-export default async function handler(req, res) {
-  const apiKey = process.env.PUBLIC_DISCOGS_API_TOKEN;
-  try {
-    const collection = await fetchCollection(apiKey);
-    res.status(200).json(collection);
-  } catch (error) {
-    console.error("Error fetching collection:", error);
-    res.status(500).json({ error: "Failed to fetch collection" });
-  }
-}
+// export default async function handler(req, res) {
+//   const apiKey = process.env.PUBLIC_DISCOGS_API_TOKEN;
+//   try {
+//     const collection = await fetchCollection(apiKey);
+//     res.status(200).json(collection);
+//   } catch (error) {
+//     console.error("Error fetching collection:", error);
+//     res.status(500).json({ error: "Failed to fetch collection" });
+//   }
+// }
