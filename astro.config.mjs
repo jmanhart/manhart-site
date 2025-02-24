c879020f53e04128cd428d7a0e7b89744a06a15e
John Manhart <manhartjohn@gmail.com>
2025-02-24 10:33:57 -0800

    Working thru and seeing if the build works

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,21 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless"; // Change from 'static' to 'serverless'
+import vercel from "@astrojs/vercel";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  output: "server", // Change from 'static' to 'server'
+  output: "server",
   adapter: vercel(),
   integrations: [sitemap()],
+  vite: {
+    root: process.cwd(), // Ensure Vite uses the root directory
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
+      dedupe: ["astro"], // Ensure Astro is deduplicated if multiple versions are installed
+    },
+  },
+  typescript: {
+    tsconfig: "./tsconfig.json", // Make sure this points to the root tsconfig.json
+  },
 });
